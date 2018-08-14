13fcd5c1560c3f09bf383a73fa8fb175ad7bc439
Christopher Stull <chris.stull@gmail.com>
2018-08-14 19:58:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,8 @@ def oxford_comma(array)
   oxford = ""
   if array.size == 1
     oxford = array[0]
+  elsif array.size == 2
+    oxford = "#{array[0]} and #{array[1]}"
   else
     for i in (0..(array.size - 2))
       oxford.<<(array[i])
